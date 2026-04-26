4ea201c7ae75363ccee4e9ed304b23805e5cbedc
kavanaraju <kavana@upenn.edu>
2026-04-26 12:59:54 -0400

    Fix column name and update to core table

--- tasks/transform-tax-year-assessment-bins/create_derived_tax_year_assessment_bins.sql
+++ tasks/transform-tax-year-assessment-bins/create_derived_tax_year_assessment_bins.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE TABLE `{project}.derived.tax_year_assessment_bins` AS
 
 SELECT
-    CAST(tax_year AS INT64) AS tax_year,
+    CAST(year AS INT64) AS tax_year,
     CAST(FLOOR(SAFE_CAST(market_value AS FLOAT64) / 25000) * 25000 AS INT64) AS lower_bound,
     CAST((FLOOR(SAFE_CAST(market_value AS FLOAT64) / 25000) + 1) * 25000 AS INT64) AS upper_bound,
     COUNT(*) AS property_count
-FROM `{project}.source.opa_assessments`
-WHERE market_value IS NOT NULL
+FROM `{project}.core.core_phl_opa_assessments`
+WHERE
+    market_value IS NOT NULL
     AND SAFE_CAST(market_value AS FLOAT64) > 0
 GROUP BY
     tax_year,
